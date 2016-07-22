c98c28b933241df5238f2cd3648d8220995f377a
Renata Antunes <tataiantunes@hotmail.com>
2016-07-21 22:33:11 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -28,9 +28,9 @@
 	var fullHeight = function() {
 
 		if ( !isMobile.any() ) {
-			$('.js-fullheight').css('height', $(window).height());
+			$('.js-fullheight').css('height', $(window).height()/2);
 			$(window).resize(function(){
-				$('.js-fullheight').css('height', $(window).height());
+				$('.js-fullheight').css('height', $(window).height()/2);
 			});
 		}
 
